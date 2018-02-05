9e68fba8be9c44ded3b4f515abbdc02c3d4dc7cc
Chandni Gupta <webcrazychandni@gmail.com>
2018-02-05 16:16:28 +0530

    work in SplashActivity

--- LTATTennisScore/app/src/main/java/tennisonline/ltat/com/ltattennisscore/ActivityContainer/SplashActivity.java
+++ LTATTennisScore/app/src/main/java/tennisonline/ltat/com/ltattennisscore/ActivityContainer/SplashActivity.java
@@ -1,9 +1,19 @@
 package tennisonline.ltat.com.ltattennisscore.ActivityContainer;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Handler;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,6 +24,7 @@ import android.view.animation.TranslateAnimation;
 import android.view.animation.Animation.AnimationListener;
 import android.widget.Toast;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import tennisonline.ltat.com.ltattennisscore.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -29,9 +40,11 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
          bt2=(Button) findViewById(R.id.bt2);
-        Toast.makeText(this, "this is harshit", Toast.LENGTH_SHORT).show();
-
         b = (Button)findViewById(R.id.watch);
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,14 +52,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-
-        final ImageView bounceBallImage = (ImageView) findViewById(R.id.bounceBallImage);
-        TranslateAnimation mAnimation = new TranslateAnimation(0f, 0f, 0f,50f);
-        mAnimation.setDuration(500);
-        mAnimation.setFillAfter(true);
-        mAnimation.setRepeatCount(-2);
-        mAnimation.setRepeatMode(Animation.REVERSE);
-        bounceBallImage.setAnimation(mAnimation);
 
         bt2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,5 +63,24 @@ public class SplashActivity extends AppCompatActivity {
         });
 
 
+
+        ImageView bounceBallImage = (ImageView)findViewById(R.id.bounceBallImage);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.tennisball);
+        RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(),bitmap);
+        roundedBitmapDrawable.setCircular(true);
+        bounceBallImage.setImageDrawable(roundedBitmapDrawable);
+        TranslateAnimation mAnimation = new TranslateAnimation(0.0f, 0.0f, 0.0f,-300.0f);
+        //TranslateAnimation mAnimation = new TranslateAnimation(0,0,0,getDisplayHeight()/5);
+        mAnimation.setDuration(500);
+        mAnimation.setFillAfter(true);
+        mAnimation.setRepeatCount(-200);
+        mAnimation.setRepeatMode(Animation.REVERSE);
+        bounceBallImage.setAnimation(mAnimation);
+
+
     }
+    private int getDisplayHeight(){
+        return this.getResources().getDisplayMetrics().heightPixels;
+    }
+
 }
